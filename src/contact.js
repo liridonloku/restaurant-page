2d3ffdeb5a4a7fb827b02bc432c8fe4f5994228e
Liridon Loku <liridonloku@gmail.com>
2021-09-11 18:11:42 +0200

    Fix scrollbar moving the page slightly.

--- src/contact.js
+++ src/contact.js
@@ -1,11 +1,15 @@
 export default function contactPane(main){
     const contactInfo = document.createElement('div');
+    contactInfo.classList.add('contact-info');
+    const contact = document.createElement('h1');
+    contact.textContent = "Contact: ";
     const phone = document.createElement('p');
     phone.textContent = "Phone no: +123-45678-9";
     const address = document.createElement('p');
-    address.textContent = "P.Sherman, 42, Wallaby Way, Sydney!"
+    address.textContent = "Address: P. Sherman, 42, Wallaby Way, Sydney!"
     const email = document.createElement('p');
-    email.textContent = "myrestaurant@restaurant.com"
+    email.textContent = "Email: myrestaurant@restaurant.com";
+    contactInfo.appendChild(contact);
     contactInfo.appendChild(phone);
     contactInfo.appendChild(address);
     contactInfo.appendChild(email);
