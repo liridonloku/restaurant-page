d979e7177e9323edcaf84ab0c75d8b2a8626119d
Liridon Loku <liridonloku@gmail.com>
2021-09-11 17:54:13 +0200

    Add contact pane content

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,13 @@
 export default function contactPane(main){
     const contactInfo = document.createElement('div');
-    const text = document.createElement('p');
-    text.textContent = "This is the contact pane!";
-    main.appendChild(text);
+    const phone = document.createElement('p');
+    phone.textContent = "Phone no: +123-45678-9";
+    const address = document.createElement('p');
+    address.textContent = "P.Sherman, 42, Wallaby Way, Sydney!"
+    const email = document.createElement('p');
+    email.textContent = "myrestaurant@restaurant.com"
+    contactInfo.appendChild(phone);
+    contactInfo.appendChild(address);
+    contactInfo.appendChild(email);
+    main.appendChild(contactInfo);
 }
